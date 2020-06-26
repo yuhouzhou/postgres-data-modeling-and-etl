8b08ae6202baefe1d2c9f9d8939e18a47d91db72
Yuhou Zhou <zyh_cn@outlook.com>
2020-06-26 20:26:27 +0000

    finish project

--- sql_queries.py
+++ sql_queries.py
@@ -12,12 +12,12 @@ songplay_table_create = ("""
 
     CREATE TABLE IF NOT EXISTS songplays
     
-    (songplay_id int PRIMARY KEY,
-    start_time date,
+    (songplay_id SERIAL PRIMARY KEY,
+    start_time timestamp,
     user_id int NOT NULL,
     level text,
     song_id text,
-    artists_id text,
+    artist_id text,
     session_id int,
     location text,
     user_agent text)
@@ -78,10 +78,10 @@ time_table_create = ("""
 
 songplay_table_insert = ("""
     
-    INSERT INTO users
-    (user_id, first_name, last_name, gender, level)
-    VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT (user_id) DO NOTHING;
+    INSERT INTO songplays
+    (start_time, user_id, level, artist_id, song_id, session_id, location, user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (songplay_id) DO NOTHING;
     
 """)
 
